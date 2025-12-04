dafd11cb3f8e4ece6603c1b272a3eb146c075c85
Paul Graggs <pagraggs1987@berkeley.edu>
2025-12-04 04:05:05 +0000

    Added Required Code to Repo

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <armadillo>
+#include "new_molecule.hpp"
+#include "CNDO.hpp"
+
+
+
+
+
+int main(){
+    return 0;
+}
---
+++ src/normal_modes.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <armadillo>
+#include <cmath>
+#include <fstream>
+#include <filesystem>
+#include <stdexcept>
+
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+
+#include "3D_gaussian.hpp"
+#include "new_molecule.hpp"
+#include "CNDO.hpp"
